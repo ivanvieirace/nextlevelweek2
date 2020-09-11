6675ba6cc9f0ddc08aef5f802ec05411f24d1ade
Ivan Vieira <ivanjr.ce@gmail.com>
2020-09-11 10:08:26 -0300

    Finish Create Landing Page

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -24,6 +24,44 @@ const styles = StyleSheet.create({
   titleBold: {
     fontFamily: "Poppins_600SemiBold",
   },
+
+  buttonsContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+    justifyContent: "space-between",
+  },
+
+  button: {
+    height: 150,
+    width: "48%",
+    backgroundColor: "#333",
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: "space-between",
+  },
+
+  buttonPrimary: {
+    backgroundColor: "#9871f5",
+  },
+
+  buttonSecondary: {
+    backgroundColor: "#04d361",
+  },
+
+  buttonText: {
+    fontFamily: "Archivo_700Bold",
+    color: "#fff",
+    fontSize: 18,
+  },
+
+  totalConnections: {
+    fontFamily: "Poppins_400Regular",
+    marginTop: 40,
+    color: "#d4c2ff",
+    fontSize: 12,
+    lineHeight: 20,
+    maxWidth: 140,
+  },
 });
 
 export default styles;
