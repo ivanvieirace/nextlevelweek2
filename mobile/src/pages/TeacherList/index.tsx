404d539f8c15daa676ef4b2ad2be09bd5e0def71
Ivan Vieira <ivanjr.ce@gmail.com>
2020-09-25 13:25:58 -0300

    Filters Header TeacherList

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,14 +1,59 @@
-import React from "react";
-import { View } from "react-native";
 import styles from "./styles";
+import React, { useState } from "react";
+import { Text, View, TextInput } from "react-native";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
-import { ScrollView } from "react-native-gesture-handler";
+import { BorderlessButton, ScrollView } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 
 function TeacherList() {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys Disponíveis" />
+      <PageHeader
+        title="Proffys Disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Qual a Matéria ?"
+              placeholderTextColor="#C1BCCC"
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da Semana</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o Dia ?"
+                  placeholderTextColor="#C1BCCC"
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o Horário ?"
+                  placeholderTextColor="#C1BCCC"
+                />
+              </View>
+            </View>
+          </View>
+        )}
+      </PageHeader>
       <ScrollView
         style={styles.teacherList}
         contentContainerStyle={{
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -9,6 +9,29 @@ const styles = StyleSheet.create({
     marginTop: -44,
     padding: 6,
   },
+  searchForm: {
+    marginBottom: 4,
+  },
+  label: {
+    color: "#D4C2FF",
+    fontFamily: "Poppins_400Regular",
+  },
+  input: {
+    height: 54,
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 10,
+  },
+  inputGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  inputBlock: {
+    width: "48%",
+  },
 });
 
 export default styles;
